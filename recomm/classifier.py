e1e12fef6e902157da3989da6dcb9213a8b26c32
deepblu <shangyu@deepblu.com>
2017-10-05 17:03:24 +0800

    fix bugs: to use try catch to determine the dimension of labels

--- recomm/classifier.py
+++ recomm/classifier.py
@@ -9,9 +9,9 @@ class ClassifierNN(object):
         self.features = features
         self.labels = labels
         self.features_size = self.features.shape[1]
-        if len(self.labels.shape[1:]) > 1:
+        try:
             self.labels_size = self.labels.shape[1]
-        else:
+        except:
             self.labels_size = 1
             self.labels.shape = [self.labels.shape[0], 1]
         self.sample_features = tf.placeholder(tf.float32,
